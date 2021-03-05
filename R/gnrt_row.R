941bbd2d80dc396dca2e8919a3bbc693961232b6
Zachary Rowson <Rowson.Zachary@epa.gov>
2021-03-05 17:45:23 -0500

    gnrt_row() initial commit

---
+++ R/gnrt_row.R
@@ -0,0 +1,8 @@
+#' This function produces a list in format of tcplfit2 "row"
+#' Currently limited to execution on one chemical.
+#' @param table corresponds to a data.table of DNT-60 Behavioral data
+#' for one chemical
+#' @import data.table
+gnrt_row <- function(name) {
+
+}
