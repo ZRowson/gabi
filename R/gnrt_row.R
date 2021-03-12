7afbb24f4ccddada0d697cbed25013c6d19e6687
Zachary Rowson <Rowson.Zachary@epa.gov>
2021-03-12 15:10:18 -0500

    gnrt_row() working form commit create_Endp() inital commit

--- R/gnrt_row.R
+++ R/gnrt_row.R
@@ -1,8 +1,27 @@
 #' This function produces a list in format of tcplfit2 "row"
 #' Currently limited to execution on one chemical.
-#' @param table corresponds to a data.table of DNT-60 Behavioral data
+#' @param table is a data.table of DNT-60 Behavioral data
 #' for one chemical
+#' @param endp is a string naming the endpoint to be evaluated
 #' @import data.table
-gnrt_row <- function(name) {
+gnrt_row <- function(table, endp) {
+  # Determine background variation
+  temp <- table[FinalConc == 0]
+  bmad <- stats::mad(temp[[endp]])
+  onesd <- stats::sd(temp[[endp]])
+  # 3 is an arbitrary value... How do we determine a better one?
+  cutoff <- 3*bmad
 
+  # Consolidate test data
+  temp <- table[FinalConc != 0]
+  conc <- temp$`FinalConc`
+  resp <- temp[[endp]]
+
+  # Generate row vector
+  row <- list(conc = conc,
+              resp = resp,
+              bmed = 0,
+              cutoff = cutoff,
+              onesd = onesd)
+  return(row)
 }
